dd2551c241fdaf1fe7c069393cae3b7fa53e47e7
nikitha kanakam <nikitha.kanakam@comfortclick.co.uk>
2024-08-13 14:27:44 +0530

    added new changes

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,7 @@
-import { MiddlewareConfigType } from "./types";
+// middleware.ts
 import { NextRequest, NextResponse } from "next/server";
 
-export const config: MiddlewareConfigType = {
-  matcher: ["/api/:path*"],
-};
+const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:8000"]; // Add more origins if needed
 
 const apiRoutes = (pathname: string) => {
   return pathname.startsWith("/api/redis");
@@ -13,15 +11,39 @@ const validApiAccessToken = (token: string) => {
   return token === (process.env.API_ACCESS_TOKEN as string);
 };
 
-export async function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl;
+export async function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+  const origin = request.headers.get("Origin");
+
+  if (origin && allowedOrigins.includes(origin)) {
+    response.headers.set("Access-Control-Allow-Origin", origin);
+  } else {
+    response.headers.set("Access-Control-Allow-Origin", "*");
+  }
+
+  response.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, access-token"
+  );
+
+  if (request.method === "OPTIONS") {
+    // response.status = 204; // No content
+    return response;
+  }
+
+  const { pathname } = request.nextUrl;
 
   if (
     apiRoutes(pathname) &&
-    !validApiAccessToken(req.headers.get("access-token") ?? "")
+    !validApiAccessToken(request.headers.get("access-token") ?? "")
   ) {
     return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
   }
 
-  return NextResponse.next();
+  return response;
 }
+
+export const config = {
+  matcher: "/api/:path*", // Adjust to apply middleware to the correct routes
+};
